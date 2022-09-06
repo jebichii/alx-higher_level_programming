1ab84a91a980af3e3d3ad4f5262d239a10be5e2f
jebichii <jebichiijoyce@gmail.com>
2022-09-06 01:23:09 -0700

    multi language

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
-
 #!/usr/bin/node
-const text = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(text);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
